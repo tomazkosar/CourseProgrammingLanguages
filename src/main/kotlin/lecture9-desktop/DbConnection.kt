4c9564f6a6a1874bc5ef3f14d7b404f62e9c37f0
Tomaz Kosar <tomaz.kosar@um.si>
2022-05-06 14:51:30 +0200

    Compose for Desktop

---
+++ src/main/kotlin/lecture9-desktop/DbConnection.kt
@@ -0,0 +1,32 @@
+import com.mongodb.client.*
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.getCollection
+
+data class Item(val _id: String, val name: String, var done: Boolean, val category: String, val username: String)
+
+data class User(val _id: String, val name: String, val password: String, val username: String)
+
+data class Category(val _id: String, val name: String)
+
+class DbConnection(url: String, dbName: String) {
+    private val client: MongoClient
+    private val database: MongoDatabase
+
+    init {
+        client = KMongo.createClient(url)
+        database = client.getDatabase(dbName)
+    }
+
+    fun getUsers(): MongoCollection<User> = database.getCollection<User>("users")
+
+    fun getCategories(): MongoCollection<Category> = database.getCollection<Category>("categories")
+
+    fun getItems(): MongoCollection<Item> = database.getCollection<Item>("items")
+
+    fun <T> printCollection(mc: MongoCollection<T>) {
+        for (doc in mc.find())
+            println(doc.toString())
+        println()
+    }
+}
+
---
+++ src/main/kotlin/lecture9-desktop/Main.kt
@@ -0,0 +1,132 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.mongodb.client.FindIterable
+import com.mongodb.client.MongoCollection
+import org.litote.kmongo.*
+
+
+@Composable
+fun RowScope.tableCell(
+    text: String,
+    color: Color,
+    weight: Float
+) {
+    Text(
+        text = text,
+        Modifier
+            .border(1.dp, Color.Black)
+            .weight(weight)
+            .padding(8.dp),
+        color = color
+    )
+}
+
+@Composable
+fun tableScreen(items: FindIterable<Item>) {
+    val tableData = (items).mapIndexed { index, item ->
+        index to item
+    }
+
+    val column1Weight = .1f
+    val column2Weight = .65f
+    val column3Weight = .15f
+    val column4Weight = .1f
+
+    LazyColumn(Modifier.fillMaxSize().padding(50.dp)) {
+
+        item {
+            Row(Modifier.background(MaterialTheme.colors.primary)) {
+                tableCell(text = "#", weight = column1Weight, color = MaterialTheme.colors.onPrimary)
+                tableCell(text = "Task title", weight = column2Weight, color = MaterialTheme.colors.onPrimary)
+                tableCell(text = "Category", weight = column3Weight, color = MaterialTheme.colors.onPrimary)
+                tableCell(text = "Done", weight = column4Weight, color = MaterialTheme.colors.onPrimary)
+            }
+        }
+
+        items(tableData) {
+            val (id, item) = it
+            Row(Modifier.fillMaxWidth()) {
+                tableCell(text = id.toString(), weight = column1Weight, color = Color.Black)
+                tableCell(text = item.name, weight = column2Weight, color = Color.Black)
+                tableCell(text = item.category, weight = column3Weight, color = Color.Black)
+                tableCell(text = if (item.done) "Yes" else "No", weight = column4Weight, color = Color.Black)
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun app(items: MongoCollection<Item>) {
+    val text1 by remember { mutableStateOf("All notes") }
+    val text2 by remember { mutableStateOf("Shopping notes") }
+    val text3 by remember { mutableStateOf("Work notes") }
+    val text4 by remember { mutableStateOf("Update first shopping notes") }
+
+    val itemList = mutableStateOf(items.find())
+
+    MaterialTheme {
+        Row(modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button( modifier = Modifier.padding(horizontal = 12.dp), onClick = {
+                itemList.value = items.find()
+            }) {
+                Text(text1)
+            }
+            Button( modifier = Modifier.padding(horizontal = 12.dp), onClick = {
+                itemList.value = items.find(Item::category eq "Shopping")
+            }) {
+                Text(text2)
+            }
+            Button( modifier = Modifier.padding(horizontal = 12.dp), onClick = {
+                itemList.value = items.find(Item::category eq "Work")
+            }) {
+                Text(text3)
+
+            }
+            Button( modifier = Modifier.padding(horizontal = 12.dp), onClick = {
+                val item: Item? = items.findOne(Item::category eq "Shopping")
+                items.updateOne(Item::category eq "Shopping", setValue(Item::done, !(item!!.done)))
+                itemList.value = items.find(Item::category eq "Shopping")
+            }) {
+                Text(text4)
+
+            }
+        }
+        tableScreen(itemList.value)
+    }
+
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication, title = "My Compose Desktop App") {
+        //connect
+        val db = DbConnection(
+            "mongodb+srv://$dbAuth@cluster0.q3d7o.gcp.mongodb.net/myFirstDatabase?retryWrites=true\n",
+            "NotesDB"
+        )
+
+        //items
+        val items = db.getItems()
+
+        //composing app
+        app(items)
+    }
+}
